7f7e26cca8b7ec80215aa30bc23adce765748285
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-04-24 14:31:28 +0700

    tính năng bộ lọc cho trang danh sách tài khoản

--- controllers/admin/accounts.controller.js
+++ controllers/admin/accounts.controller.js
@@ -1,18 +1,30 @@
 const Account = require('../../models/account.model');
 
+// helper
+const filterhelper = require('../../helper/filter.helper');
+
 // [GET] /admin/accounts
 module.exports.index = async (req, res) => {
     try{
-        const records = await Account.find({
+        const findObject = {
             deleted: false
-        });
+        }
 
-        const numberOfRecords = await Account.countDocuments({deleted: false});
+        // filter status
+        const buttonFilterStatus = filterhelper.buttonStatus(req.query);
+        if(req.query.status){
+            findObject.status = req.query.status;
+        }
+
+        const records = await Account.find(findObject);
+
+        const numberOfRecords = await Account.countDocuments(findObject);
 
         res.render('admin/pages/accounts/index', {
             title: "Danh sách tài khoản",
             records: records,
-            numberOfRecords: numberOfRecords
+            numberOfRecords: numberOfRecords,
+            buttonFilterStatus: buttonFilterStatus
         });
     }
     catch(error){
