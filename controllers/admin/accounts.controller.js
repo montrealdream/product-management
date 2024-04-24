12bd007d7a6c405fe314bc7b3d14b0881348e1f3
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-04-24 14:35:14 +0700

    tính năng pagination cho danh sách tài khoản

--- controllers/admin/accounts.controller.js
+++ controllers/admin/accounts.controller.js
@@ -2,6 +2,7 @@ const Account = require('../../models/account.model');
 
 // helper
 const filterhelper = require('../../helper/filter.helper');
+const paginationHelper = require('../../helper/pagination.helper');
 
 // [GET] /admin/accounts
 module.exports.index = async (req, res) => {
@@ -16,15 +17,23 @@ module.exports.index = async (req, res) => {
             findObject.status = req.query.status;
         }
 
-        const records = await Account.find(findObject);
-
+        // count document
         const numberOfRecords = await Account.countDocuments(findObject);
+
+        // pagination
+        const paginationObject = paginationHelper(req.query, 5, numberOfRecords);
+
+
+        const records = await Account.find(findObject)
+                                     .limit(paginationObject.limit)
+                                     .skip(paginationObject.skip);
 
         res.render('admin/pages/accounts/index', {
             title: "Danh sách tài khoản",
             records: records,
             numberOfRecords: numberOfRecords,
-            buttonFilterStatus: buttonFilterStatus
+            buttonFilterStatus: buttonFilterStatus,
+            pagination: paginationObject
         });
     }
     catch(error){
