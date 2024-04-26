006a9dc05ad7956812a363e1e9fa43b8eaf8e771
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-04-26 09:41:30 +0700

    cloudinary

--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -46,29 +46,35 @@ router.post(
     upload.single('thumbnail'),
     // function upload onto cloudinary
     function (req, res, next) {
-        let streamUpload = (req) => {
-            return new Promise((resolve, reject) => {
-                let stream = cloudinary.uploader.upload_stream(
-                  (error, result) => {
-                    if (result) {
-                      resolve(result);
-                    } else {
-                      reject(error);
-                    }
-                  }
-                );
-    
-              streamifier.createReadStream(req.file.buffer).pipe(stream);
-            });
-        };
-    
-        async function upload(req) {
-            let result = await streamUpload(req);
-            console.log(result);
+      // if u have upload image (upload.single(...) will create req.file)
+        if(req.file){
+            let streamUpload = (req) => {
+                return new Promise((resolve, reject) => {
+                    let stream = cloudinary.uploader.upload_stream((error, result) => {
+                        if (result) {
+                          resolve(result);
+                        }
+                        else {
+                          reject(error);
+                        }
+                      }
+                    );
+                  streamifier.createReadStream(req.file.buffer).pipe(stream);
+                });
+            };
+        
+            async function upload(req) {
+                let result = await streamUpload(req);
+                console.log(result);
+            }
+        
+            upload(req);
+            next();
         }
-    
-        upload(req);
-        next();
+        else{
+            // next();
+        }
+        
     },
     validate.createProduct,
     controller.createProduct
