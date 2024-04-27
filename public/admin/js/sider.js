6624e7aa6e26e9715344fa2e2b90cc065953b779
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-04-27 18:10:02 +0700

    sub sider cho các trang, sub sider create

--- public/admin/js/sider.js
+++ public/admin/js/sider.js
@@ -4,40 +4,22 @@ const bodyUserDefine = document.querySelector(".body");
 const expand_btn = bodyUserDefine.querySelector(".expand-btn");
 
 // // click sider
-const siderItem = bodyUserDefine.querySelectorAll(".sider-body li a");
+const siderItem = bodyUserDefine.querySelectorAll(".sider-body li");
 if(siderItem.length > 0){
     const url = window.location.href;
     // scan
-    siderItem.forEach((item) => {
-        if(item.href === url){
-            const parent = item.closest('li');
+    siderItem.forEach((item, index) => {
+        const a = item.querySelector('a');
+        if(a.href === url){
+            const parentOfLi = siderItem[index+1].closest('ul');
+            const parent = a.closest('li');
+
             parent.classList.add("sider--active");
-            console.log(item);
+            parentOfLi.classList.add("open-sub-sider");
         }   
     });
 }
 
-// collapse sider
-const siderItemLi = bodyUserDefine.querySelectorAll(".sider-body li");
-if(siderItemLi.length > 0){
-    // scan
-    siderItemLi.forEach((item, index) => {
-        item.addEventListener("mouseover", (event) => {
-            const parentOfLi = siderItemLi[index+1].closest('ul');
-            parentOfLi.classList.add("open-sub-sider");
-
-            
-        });
-        item.addEventListener("mouseout", (event) => {
-            const parentOfLi = siderItemLi[index+1].closest('ul');
-            setTimeout(() => {
-                parentOfLi.classList.remove("open-sub-sider");
-            }, 1000);
-            
-            
-        });
-    });
-}
 
 // collapse sider
 expand_btn.addEventListener("click", () => {
