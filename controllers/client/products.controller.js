75bbb196d7fc70bcd9e525b177660c40c1c34d53
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-04-27 22:56:19 +0700

    xong tính năng truy cập vào danh mục lấy sản phẩm

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -42,27 +42,70 @@ module.exports.index = async (req, res) => {
 
 // [GET] /products/:slugCategory
 module.exports.categoryView = async (req, res) => {
+    // function get category consist of child category & connect in one array
+    async function getAllRecordsOfCategory(parent_id) {
+        // findObject
+        const findObject = {
+            status: "active",
+            deleted: false
+        }
+
+        // get children category
+        const childrenCategory = await productCategory.find({
+            parent_id: parent_id,
+            ...findObject
+        });
+
+        // create new array children category with syntax spread
+        let category = [...childrenCategory];
+
+        // scan & get all children category
+        for(const child of childrenCategory){
+            const children = await getAllRecordsOfCategory(child.id);
+            // connect to array (nghiã là nối tất cả danh mục con và cha vào cùng 1 mảng)
+            category = category.concat(children);
+        }
+
+        // return array 
+        return category;
+    }
+
     try{
         const findObject = {
             status: "active",
             deleted: false
         }
 
-        // get category need to find
-        const getCategory = await productCategory.findOne({
+        // get category need to find (root category you click)
+        const getCategoryBySlugCategory = await productCategory.findOne({
             ...findObject,
             slug: req.params.slugCategory
-        });
+        }); 
+
+        // get all children category
+        const category = await getAllRecordsOfCategory(getCategoryBySlugCategory.id);
+        // get all id of category child
+        const idCategory = category.map(item => item.id);
 
         // count document
         const numberofRecords = await Product.countDocuments({
-            product_category_id: getCategory.id,
+            product_category_id: {
+                $in: [
+                    getCategoryBySlugCategory.id,
+                    ...idCategory
+                ]
+            },
             ...findObject
         });
 
         // get records of category
         const data = await Product.find({
-            product_category_id: getCategory.id,
+            product_category_id: {
+                $in: [
+                    getCategoryBySlugCategory.id,
+                    ...idCategory
+                ]
+            },
             ...findObject
         });
         
