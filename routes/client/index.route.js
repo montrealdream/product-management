129e52abef3cab0b00641e469bdd6e6df1adc972
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-04-29 15:54:10 +0700

    đặt middleware của category client ra chung

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,18 +3,21 @@ const homeRouter = require('./home.route');
 const productRouter = require('./products.route');
 
 // middleware
-const middleware = require('../../middleware/client/category.middleware');
+const middlewareCategory = require('../../middleware/client/category.middleware');
 
 module.exports = (app) => {
+    // chạy qua middleware category trước
+    app.use(middlewareCategory.category);
+
     app.use(
         '/', 
-        middleware.category,
+        // middleware.category,
         homeRouter
     );
 
     app.use(
         '/products', 
-        middleware.category,
+        // middleware.category,
         productRouter
     );
 }
