5b4b67ca335a4d84032521fb4085208279d2d58c
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-04-29 20:57:44 +0700

    dùng helper đã viết từ trước để đóng gói search keyword, vẫn giữ keyword đã tìm trên thanh tìm kiếm

--- controllers/client/search.controller.js
+++ controllers/client/search.controller.js
@@ -3,6 +3,7 @@ const Product = require('../../models/product.model');
 
 // helper
 const productHelper = require('../../helper/products.helper');
+const searchHelper = require('../../helper/search.helper');
 
 // [GET] /search/
 module.exports.index = async (req, res) => {
@@ -12,23 +13,17 @@ module.exports.index = async (req, res) => {
     }
 
     try{
-        // get keyword
-        const keyword = req.query.keyword;
-
-        //regex keyword
-        const regexKeyword = new RegExp(keyword, "i");
+        // get keyword & regex
+        const keywordObject = searchHelper(req.query);
+        if(req.query.keyword){
+            findObject.title = keywordObject.title;
+        }
         
         // count document
-        const numberofRecords = await Product.countDocuments({
-            title: regexKeyword,
-            ...findObject
-        });
+        const numberofRecords = await Product.countDocuments(findObject);
 
         // get document
-        const data= await Product.find({
-            title: regexKeyword,
-            ...findObject
-        });
+        const data= await Product.find(findObject);
 
         // calc discount
         const records = productHelper.discountMany(data);
@@ -36,7 +31,8 @@ module.exports.index = async (req, res) => {
         res.render('client/pages/search/index', {
             title: "Kết quả tìm kiếm",
             numberofRecords: numberofRecords,
-            records: records
+            records: records,
+            keyword: keywordObject.keyword
         });
     }
     catch(error){
