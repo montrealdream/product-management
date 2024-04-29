5e8f922c2ffea856942acd711384ae149226c838
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-04-29 22:50:07 +0700

    tạo schema cho cart

---
+++ models/cart.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// Defining a model
+const cartSchema = new mongoose.Schema(
+    {
+        user_id: String,
+        products: [
+            {
+                product_id: String,
+                quantity: Number
+            }
+        ]
+    }
+);
+
+// Accessing a Model
+const Cart = mongoose.model('Cart', cartSchema, 'carts');
+
+// export
+module.exports = Cart;
