88c36992643705b5d47f6a5b370e30d8a21d398a
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-05-05 11:25:36 +0700

    model cài đặt chung cho trang admin

---
+++ models/setting-general.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// Defining a model
+const settingGeneralSchema = new mongoose.Schema(
+    {
+        // lưu ý luôn luôn trong collection chỉ có 1 bản ghi này
+        websiteNam: String,
+        logo: String,
+        email: String,
+        tel: String,
+        address: String,
+        copyright: String
+    },
+    {
+        timestamps: true
+    }
+);
+
+// Accessing a Model
+const SettingGeneral = mongoose.model('SettingGeneral', settingGeneralSchema, 'setting-general');
+
+// export
+module.exports = SettingGeneral;
