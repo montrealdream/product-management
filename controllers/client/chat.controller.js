89f8e36f6ed925ae9aa37f489150fcbd6979af6d
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-05-16 21:54:58 +0700

    đóng gói tính năng chat socket

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,76 +1,19 @@
 // model
 const Chat = require('../../models/chat.model');
-const User = require('../../models/user.model');
 
 // helper
 const uploadToCloudinary = require('../../helper/uploadToCloudinary.helper');
 
+// socket
+const chatSocket = require('../../socket/client/chat.socket');
+
 // [GET] /chat
 module.exports.index = async (req, res) => {
     try{
+        // socket
+        chatSocket(req, res);
+        // end socket
 
-        const userId = res.locals.user.id;
-        const userFullName = res.locals.user.fullName;
-
-        // get avatar of client
-        const user = await User.findOne({_id: userId});
-        if(!user.avatar){
-            user.avatar = "https://vnn-imgs-a1.vgcloud.vn/image1.ictnews.vn/_Files/2020/03/17/trend-avatar-1.jpg";
-        }
-
-        // LISTEN CLIENT CONNECT "ONLINE"
-        _io.once('connection', (socket) => {
-            // inform user connect
-            console.log(`${userFullName} đang hoạt động`);
-
-            // inform user disconnect
-            socket.on('disconnect', () => {
-                console.log(`${userFullName} dừng hoạt động`);
-            });
-
-           
-            // CLIENT SEND MESSAGE
-            socket.on("CLIENT_SEND_MESSAGE", async (obj) => {
-                let imagesArray = []; //empty array
-
-                const content = obj.content; // get content
-                
-                // create link image on cloudinary
-                for(const image of obj.images){
-                    const linkImg = await uploadToCloudinary(image);
-                    imagesArray.push(linkImg);
-                }
-                
-                // create & save
-                const chat = new Chat({
-                    user_id: userId,
-                    content: content,
-                    images: imagesArray
-                });
-                await chat.save();
-
-                // SERVER RETURN MESSAGE
-                _io.emit("SERVER_RETURN_MESSAGE", {
-                    user_id: userId,
-                    user_name: userFullName,
-                    content: content,
-                    avatar: user.avatar,
-                    images: imagesArray //array contain link img
-                });
-            });
-            
-            // CLIENT SEND TYPING
-            socket.on("CLIENT_SEND_TYPING", (type) => {
-                // SEVER SEND TYPING
-                socket.broadcast.emit("SERVER_RETURN_TYPING", {
-                    user_id: userId,
-                    user_name: userFullName,
-                    avatar: user.avatar,
-                    type: type
-                });
-             });
-        });
-        
         // get & render views
         const chats = await Chat.find({deleted: false});
         for(const chat of chats){
---
+++ socket/client/chat.socket.js
@@ -0,0 +1,70 @@
+// model
+const User = require('../../models/user.model');
+const Chat = require('../../models/chat.model');
+
+module.exports =  async (req, res) => {
+    // my user
+    const userId = res.locals.user.id;
+    const userFullName = res.locals.user.fullName;
+
+    // get avatar of client
+    const user = await User.findOne({_id: userId});
+    if(!user.avatar){
+        user.avatar = "https://vnn-imgs-a1.vgcloud.vn/image1.ictnews.vn/_Files/2020/03/17/trend-avatar-1.jpg";
+    }
+
+    // LISTEN CLIENT CONNECT "ONLINE"
+    _io.once('connection', (socket) => {
+        // inform user connect
+        console.log(`${userFullName} đang hoạt động`);
+
+        // inform user disconnect
+        socket.on('disconnect', () => {
+            console.log(`${userFullName} dừng hoạt động`);
+        });
+
+    
+        // CLIENT SEND MESSAGE
+        socket.on("CLIENT_SEND_MESSAGE", async (obj) => {
+            let imagesArray = []; //empty array
+
+            const content = obj.content; // get content
+            
+            // create link image on cloudinary
+            for(const image of obj.images){
+                const linkImg = await uploadToCloudinary(image);
+                imagesArray.push(linkImg);
+            }
+        
+            // create & save
+            const chat = new Chat({
+                user_id: userId,
+                content: content,
+                images: imagesArray
+            });
+            await chat.save();
+
+            // SERVER RETURN MESSAGE
+            _io.emit("SERVER_RETURN_MESSAGE", {
+                user_id: userId,
+                user_name: userFullName,
+                content: content,
+                avatar: user.avatar,
+                images: imagesArray //array contain link img
+            });
+        });
+        // END CLIENT SEND MESSAGE
+    
+        // CLIENT SEND TYPING
+        socket.on("CLIENT_SEND_TYPING", (type) => {
+            // SEVER SEND TYPING
+            socket.broadcast.emit("SERVER_RETURN_TYPING", {
+                user_id: userId,
+                user_name: userFullName,
+                avatar: user.avatar,
+                type: type
+            });
+        });
+        // END CLIENT SEND TYPING
+    });
+}
