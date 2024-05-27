9e3a12329a327b3072de7d9191aaf289c5419b6f
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-05-27 20:02:22 +0700

    hiển thị những người trong dsach bạn bè để tạo phòng chat

--- controllers/client/rooms-chat.controller.js
+++ controllers/client/rooms-chat.controller.js
@@ -19,8 +19,18 @@ module.exports.index = async (req, res) => {
 // [GET] /rooms-chat/create
 module.exports.createView = async (req, res) => {
     try{
+        // lấy tất cả bạn bè, để có thể thêm vào NHÓM
+        const listFriendID = res.locals.user.listFriend.map(friend => friend.user_id);
+        
+        const listFriendInfo = await User.find({
+            _id: {$in: listFriendID},
+            status: "active",
+            deleted: false,
+        }).select("fullName avatar email tel");
+
         res.render('client/pages/rooms-chat/create', {
-            title : "Tạo phòng"
+            title : "Tạo phòng",
+            listFriendInfo
         });
     }
     catch(error){
